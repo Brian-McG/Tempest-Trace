61a7811b7577e13087f6a3db9ab738fcadcae548
Jacques Heunis <jaaksemail@gmail.com>
2015-08-15 17:02:37 +0200

    Add basic pre-defined motions (E.g obstacle climb)

--- Assets/Scripts/Player Controller/FirstPersonMovement.cs
+++ Assets/Scripts/Player Controller/FirstPersonMovement.cs
@@ -1,72 +1,172 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+using System.Collections.Generic;
+
+enum DefinedMotion
+{
+    NONE,
+    CLIMB,
+    VAULT
+}
+
 public class FirstPersonMovement : MonoBehaviour
 {
-  public float RunSpeed;
-  public float JumpForce;
+    public float RunSpeed;
+    public float JumpForce;
 
-  private Animator animator;
-  private int animParamJump;
-  private int animParamSpeed;
+    private Animator animator;
+    private int animParamJump;
+    private int animParamSpeed;
 
-  private CharacterController charController;
-  private bool isGrounded;
-  private float yVelocity;
+    private CharacterController charController;
+    private bool isGrounded;
+    private Vector3 velocity;
 
-  void Awake()
-  {
-    charController = GetComponent<CharacterController>();
+    private DefinedMotion currentMotion;
+    private List<Vector3> motionTargets;
+    private int motionProgress;
 
-    animator = GetComponentInChildren<Animator>();
-    animParamJump = Animator.StringToHash("Jump");
-    animParamSpeed = Animator.StringToHash("Speed");
-  }
-
-  // TODO: May be worthwhile moving movement to FixedUpdate for better physics interaction
-  //       If we do we might want to store movement input each frame lest we miss any
-  void Update()
-  {
-    float zMovement = Input.GetAxis("Vertical");
-    float xMovement = Input.GetAxis("Horizontal");
-    Vector3 moveVector = new Vector3(xMovement, 0, zMovement);
-
-    if (moveVector != Vector3.zero)
+    void Awake()
     {
-      float moveMagnitude = moveVector.magnitude;
-      moveVector /= moveMagnitude;
-      moveMagnitude = Mathf.Min(1.0f, moveMagnitude);
+        charController = GetComponent<CharacterController>();
 
-      moveVector = (transform.rotation * moveVector) *
-        RunSpeed * moveMagnitude;
+        animator = GetComponentInChildren<Animator>();
+        animParamJump = Animator.StringToHash("Jump");
+        animParamSpeed = Animator.StringToHash("Speed");
+
+        velocity = Vector3.zero;
+        isGrounded = true;
+        currentMotion = DefinedMotion.NONE;
+        motionTargets = new List<Vector3>();
     }
 
+    private void updateOnPlayerInput()
+    {
+        // Disallow change of movement vector while in the air
+        if(isGrounded)
+        {
+            float zMovement = Input.GetAxis("Vertical");
+            float xMovement = Input.GetAxis("Horizontal");
+            Vector3 moveVector = new Vector3(xMovement, 0, zMovement);
 
-    bool shouldJump = Input.GetKeyDown(KeyCode.Space);
-    if (isGrounded && shouldJump)
-    {
-      yVelocity = JumpForce;
-      //animator.SetBool(animParamJump, true);
-    } else
-    {
-      yVelocity -= 9.81f * Time.deltaTime;
+            if (moveVector != Vector3.zero)
+            {
+                float moveMagnitude = moveVector.magnitude;
+                moveVector /= moveMagnitude;
+                moveMagnitude = Mathf.Min(1.0f, moveMagnitude);
+
+                moveVector = (transform.rotation * moveVector) * RunSpeed * moveMagnitude;
+            }
+            velocity.x = moveVector.x;
+            velocity.z = moveVector.z;
+
+            bool shouldJump = Input.GetKeyDown(KeyCode.Space);
+            Debug.DrawLine(transform.position, transform.position+transform.forward, Color.green);
+            if (shouldJump)
+            {
+                // Check for climbable obstacles
+                RaycastHit climbHitInfo;
+                if(Physics.Raycast(transform.position, transform.forward, out climbHitInfo, 1.0f))
+                {
+                    Debug.Log("Jump next to an obstacle");
+                    Vector3 climbHitPoint = climbHitInfo.point;
+                    Vector3 maxClimbCeiling = climbHitPoint + new Vector3(0.0f, 2.0f, 0.0f);
+
+                    if(Physics.Raycast(maxClimbCeiling, new Vector3(0.0f, -1.0f, 0.0f),
+                                       out climbHitInfo, 2.0f))
+                    {
+                        Vector3 climbTarget = climbHitInfo.point +
+                                                new Vector3(0.0f, 1.1f, 0.0f) +
+                                                (transform.forward * 0.5f);
+                        Vector3 climbMidpoint = transform.position;
+                        climbMidpoint.y = climbTarget.y;
+
+                        currentMotion = DefinedMotion.CLIMB;
+                        motionTargets.Clear();
+                        motionTargets.Add(climbMidpoint);
+                        motionTargets.Add(climbTarget);
+                        motionProgress = 0;
+                        Debug.Log("Begin Motion");
+                    }
+                }
+
+                velocity.y = JumpForce;
+                //animator.SetBool(animParamJump, true);
+            } else
+            {
+                velocity.y -= 9.81f * Time.deltaTime;
+            }
+        }
+        else
+        {
+            velocity.y -= 9.81f * Time.deltaTime;
+        }
+
+        Vector3 preMoveLoc = transform.position;
+        charController.Move(velocity * Time.deltaTime);
+        Vector3 postMoveLoc = transform.position;
+        Vector3 actualMoveOffset = postMoveLoc - preMoveLoc;
+        Vector3 actualMoveVelocity = actualMoveOffset * (1.0f/Time.deltaTime);
+
+        // Update velocity if we got blocked somewhere along the way
+        if(velocity.sqrMagnitude - actualMoveVelocity.sqrMagnitude > 0.01f)
+        {
+            velocity = actualMoveVelocity;
+        }
+
+        // TODO: What if we couldnt move the full distance?
+        //       - We might have hit a roof (set Y velocity to 0)
+        //       - We might have hit an obstacle (so we didnt run as far as we thought)
+
+        bool wasGrounded = isGrounded;
+        isGrounded = false;
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, new Vector3(0, -1, 0),
+                               out hitInfo, 1.02f))
+        {
+            if(!wasGrounded)
+                Debug.Log("GROUNDED");
+            isGrounded = true;
+            //animator.SetBool(animParamJump, false);
+            velocity.y = 0.0f;
+        }
+
     }
 
-    moveVector += new Vector3(0.0f, yVelocity, 0.0f);
-
-    charController.Move(moveVector * Time.deltaTime);
-    // TODO: What if we couldnt move the full distance?
-    //       - We might have hit a roof (set Y velocity to 0)
-    //       - We might have hit an obstacle (so we didnt run as far as we thought)
-
-    isGrounded = false;
-    RaycastHit hitInfo;
-    if (Physics.Raycast(transform.position, new Vector3(0, -1, 0),
-                           out hitInfo, 1.02f, (1 << 8) | (1 << 10)))
+    private void updateOnCurrentMotion()
     {
-      isGrounded = true;
-      //animator.SetBool(animParamJump, false);
-      yVelocity = 0.0f;
+        float motionSpeed = 6.0f * Time.deltaTime;
+        Vector3 targetOffset = motionTargets[motionProgress] - transform.position;
+        if(targetOffset.sqrMagnitude < motionSpeed * motionSpeed)
+        {
+            transform.position = motionTargets[motionProgress];
+            ++motionProgress;
+            Debug.Log("Move on to motion progress " + motionProgress);
+
+            if(motionProgress >= motionTargets.Count)
+            {
+                currentMotion = DefinedMotion.NONE;
+                velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            }
+            return;
+        }
+
+        Vector3 targetDirection = targetOffset.normalized;
+        transform.position += targetDirection * motionSpeed;
     }
-  }
+
+    // TODO: May be worthwhile moving movement to FixedUpdate for better physics interaction
+    //       If we do we might want to store movement input each frame lest we miss any
+    void Update()
+    {
+        if(currentMotion == DefinedMotion.NONE)
+        {
+            updateOnPlayerInput();
+        }
+        else
+        {
+            updateOnCurrentMotion();
+        }
+    }
 }
