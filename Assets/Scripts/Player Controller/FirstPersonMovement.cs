9622bcb49916579cbe46eadec129d1315fdb2768
Brian Mc George <mcgbri004@myuct.ac.za>
2015-08-17 21:22:24 +0200

    :new: Add iteration one of respawn script

--- Assets/Scripts/Player Controller/FirstPersonMovement.cs
+++ Assets/Scripts/Player Controller/FirstPersonMovement.cs
@@ -9,219 +9,219 @@ using UnityEngine.EventSystems;
 
 public enum DefinedMotion
 {
-    /// <summary>
-    /// Not currently enacting a predefined motion (IE just running around)
-    /// </summary>
-    NONE,
+  /// <summary>
+  /// Not currently enacting a predefined motion (IE just running around)
+  /// </summary>
+  NONE,
 
-    /// <summary>
-    /// Climbing a tall obstacle
-    /// </summary>
-    CLIMB,
+  /// <summary>
+  /// Climbing a tall obstacle
+  /// </summary>
+  CLIMB,
 
-    /// <summary>
-    /// Vaulting over a low obstacle
-    /// </summary>
-    VAULT
+  /// <summary>
+  /// Vaulting over a low obstacle
+  /// </summary>
+  VAULT
 }
 
 public class FirstPersonMovement : MonoBehaviour
 {
-    public float RunSpeed;
-    public float JumpForce;
-    public float AirControlFactor;
+  public float RunSpeed;
+  public float JumpForce;
+  public float AirControlFactor;
+  public Vector3 Velocity;
 
-    private Animator animator;
-    private int animParamJump;
-    private int animParamSpeed;
+  private Animator animator;
+  private int animParamJump;
+  private int animParamSpeed;
 
-    private CharacterController charController;
-    private bool isGrounded;
-    private Vector3 velocity;
+  private CharacterController charController;
+  private bool isGrounded;
 
-    private DefinedMotion currentMotion;
-    private List<Vector3> motionTargets;
-    private int motionProgress;
+  private DefinedMotion currentMotion;
+  private List<Vector3> motionTargets;
+  private int motionProgress;
 
-    private void Awake()
-    {
-        charController = GetComponent<CharacterController>();
+  private void Awake()
+  {
+    charController = GetComponent<CharacterController>();
 
-        animator = GetComponentInChildren<Animator>();
-        animParamJump = Animator.StringToHash("Jump");
-        animParamSpeed = Animator.StringToHash("Speed");
+    animator = GetComponentInChildren<Animator>();
+    animParamJump = Animator.StringToHash("Jump");
+    animParamSpeed = Animator.StringToHash("Speed");
 
-        velocity = Vector3.zero;
-        isGrounded = true;
-        currentMotion = DefinedMotion.NONE;
-        motionTargets = new List<Vector3>();
-    }
+    Velocity = Vector3.zero;
+    isGrounded = true;
+    currentMotion = DefinedMotion.NONE;
+    motionTargets = new List<Vector3>();
+  }
 
-    // NOTE: We assume here that the player has height 2 and that the player's origin is at height
-    //       1 from the ground (IE in the centre of the player)
-    private void CheckForDefinedMotions()
-    {
-        float motionCheckDistance = 1.0f;
-        Vector3 currentPosition = transform.position;
-        Vector3 forwardDir = transform.forward;
+  // NOTE: We assume here that the player has height 2 and that the player's origin is at height
+  //       1 from the ground (IE in the centre of the player)
+  private void CheckForDefinedMotions()
+  {
+    float motionCheckDistance = 1.0f;
+    Vector3 currentPosition = transform.position;
+    Vector3 forwardDir = transform.forward;
         
-        float vaultCheckHeight = 0.3f;
-        float maxVaultHeight = 0.6f;
-        RaycastHit vaultCheckInfo;
-        Vector3 vaultCheckPosition = currentPosition - new Vector3(0.0f, 1.0f, 0.0f) + new Vector3(0.0f, vaultCheckHeight, 0.0f);
-        bool canVault = Physics.Raycast(vaultCheckPosition, forwardDir, out vaultCheckInfo, motionCheckDistance);
+    float vaultCheckHeight = 0.3f;
+    float maxVaultHeight = 0.6f;
+    RaycastHit vaultCheckInfo;
+    Vector3 vaultCheckPosition = currentPosition - new Vector3(0.0f, 1.0f, 0.0f) + new Vector3(0.0f, vaultCheckHeight, 0.0f);
+    bool canVault = Physics.Raycast(vaultCheckPosition, forwardDir, out vaultCheckInfo, motionCheckDistance);
         
-        float climbCheckHeight = 1.0f;
-        float maxClimbHeight = 2.0f;
-        RaycastHit climbCheckInfo;
-        Vector3 climbCheckPosition = currentPosition - new Vector3(0.0f, 1.0f, 0.0f) + new Vector3(0.0f, climbCheckHeight, 0.0f);
-        bool canClimb = Physics.Raycast(climbCheckPosition, forwardDir, out climbCheckInfo, motionCheckDistance);
+    float climbCheckHeight = 1.0f;
+    float maxClimbHeight = 2.0f;
+    RaycastHit climbCheckInfo;
+    Vector3 climbCheckPosition = currentPosition - new Vector3(0.0f, 1.0f, 0.0f) + new Vector3(0.0f, climbCheckHeight, 0.0f);
+    bool canClimb = Physics.Raycast(climbCheckPosition, forwardDir, out climbCheckInfo, motionCheckDistance);
 
-        if (canVault && !canClimb)
-        {
-            Vector3 vaultCheckPoint = vaultCheckInfo.point + (forwardDir * 0.2f);
-            Vector3 vaultCeiling = vaultCheckPoint + new Vector3(0.0f, maxVaultHeight, 0.0f);
+    if (canVault && !canClimb)
+    {
+      Vector3 vaultCheckPoint = vaultCheckInfo.point + (forwardDir * 0.2f);
+      Vector3 vaultCeiling = vaultCheckPoint + new Vector3(0.0f, maxVaultHeight, 0.0f);
 
-            if (Physics.Raycast(vaultCeiling, Vector3.down,
+      if (Physics.Raycast(vaultCeiling, Vector3.down,
                                 out vaultCheckInfo, maxClimbHeight))
-            {
-                Vector3 vaultTarget = vaultCheckInfo.point + new Vector3(0.0f, 1.0f, 0.0f) +
-                                      new Vector3(0.0f, 0.1f, 0.0f);
-                Vector3 vaultMidpoint = currentPosition;
-                vaultMidpoint.y = vaultTarget.y;
+      {
+        Vector3 vaultTarget = vaultCheckInfo.point + new Vector3(0.0f, 1.0f, 0.0f) +
+          new Vector3(0.0f, 0.1f, 0.0f);
+        Vector3 vaultMidpoint = currentPosition;
+        vaultMidpoint.y = vaultTarget.y;
 
-                Vector3 vaultEnd = currentPosition + (forwardDir * 2.0f);
-                Vector3 vaultEndMidpoint = vaultEnd;
-                vaultEndMidpoint.y = vaultTarget.y;
+        Vector3 vaultEnd = currentPosition + (forwardDir * 2.0f);
+        Vector3 vaultEndMidpoint = vaultEnd;
+        vaultEndMidpoint.y = vaultTarget.y;
 
-                currentMotion = DefinedMotion.VAULT;
-                motionTargets.Clear();
-                motionTargets.Add(vaultMidpoint);
-                motionTargets.Add(vaultTarget);
-                motionTargets.Add(vaultEndMidpoint);
-                motionTargets.Add(vaultEnd);
-                motionProgress = 0;
-            }
-        }
-        else if (canClimb)
-        {
-            Vector3 climbCheckPoint = climbCheckInfo.point + (forwardDir * 0.2f);
-            Vector3 climbCeiling = climbCheckPoint + new Vector3(0.0f, maxClimbHeight, 0.0f);
+        currentMotion = DefinedMotion.VAULT;
+        motionTargets.Clear();
+        motionTargets.Add(vaultMidpoint);
+        motionTargets.Add(vaultTarget);
+        motionTargets.Add(vaultEndMidpoint);
+        motionTargets.Add(vaultEnd);
+        motionProgress = 0;
+      }
+    }
+    else if (canClimb)
+    {
+      Vector3 climbCheckPoint = climbCheckInfo.point + (forwardDir * 0.2f);
+      Vector3 climbCeiling = climbCheckPoint + new Vector3(0.0f, maxClimbHeight, 0.0f);
 
-            if (Physics.Raycast(climbCeiling, Vector3.down,
+      if (Physics.Raycast(climbCeiling, Vector3.down,
                                 out climbCheckInfo, maxClimbHeight))
-            {
-                Vector3 climbTarget = climbCheckInfo.point + new Vector3(0.0f, 1.0f, 0.0f) +
-                                      new Vector3(0.0f, 0.1f, 0.0f);
-                Vector3 climbMidpoint = transform.position;
-                climbMidpoint.y = climbTarget.y;
+      {
+        Vector3 climbTarget = climbCheckInfo.point + new Vector3(0.0f, 1.0f, 0.0f) +
+          new Vector3(0.0f, 0.1f, 0.0f);
+        Vector3 climbMidpoint = transform.position;
+        climbMidpoint.y = climbTarget.y;
 
-                currentMotion = DefinedMotion.CLIMB;
-                motionTargets.Clear();
-                motionTargets.Add(climbMidpoint);
-                motionTargets.Add(climbTarget);
-                motionProgress = 0;
-            }
-        }
+        currentMotion = DefinedMotion.CLIMB;
+        motionTargets.Clear();
+        motionTargets.Add(climbMidpoint);
+        motionTargets.Add(climbTarget);
+        motionProgress = 0;
+      }
     }
+  }
     
-    private void UpdateOnPlayerInput()
+  private void UpdateOnPlayerInput()
+  {
+    // Disallow change of movement vector while in the air
+    float movementZ = Input.GetAxis("Vertical");
+    float movementX = Input.GetAxis("Horizontal");
+    Vector3 moveVector = new Vector3(movementX, 0, movementZ);
+
+    if (moveVector != Vector3.zero)
     {
-        // Disallow change of movement vector while in the air
-        float movementZ = Input.GetAxis("Vertical");
-        float movementX = Input.GetAxis("Horizontal");
-        Vector3 moveVector = new Vector3(movementX, 0, movementZ);
+      float moveMagnitude = moveVector.magnitude;
+      moveVector /= moveMagnitude;
+      moveMagnitude = Mathf.Min(1.0f, moveMagnitude);
 
-        if (moveVector != Vector3.zero)
-        {
-            float moveMagnitude = moveVector.magnitude;
-            moveVector /= moveMagnitude;
-            moveMagnitude = Mathf.Min(1.0f, moveMagnitude);
+      moveVector = (transform.rotation * moveVector) * RunSpeed * moveMagnitude;
+    }
 
-            moveVector = (transform.rotation * moveVector) * RunSpeed * moveMagnitude;
-        }
+    if (isGrounded)
+    {
+      Velocity.x = moveVector.x;
+      Velocity.z = moveVector.z;
+    }
+    else
+    {
+      Velocity.x = Mathf.Lerp(Velocity.x, moveVector.x, AirControlFactor);
+      Velocity.z = Mathf.Lerp(Velocity.z, moveVector.z, AirControlFactor);
+    }
 
-        if (isGrounded)
-        {
-            velocity.x = moveVector.x;
-            velocity.z = moveVector.z;
-        }
-        else
-        {
-            velocity.x = Mathf.Lerp(velocity.x, moveVector.x, AirControlFactor);
-            velocity.z = Mathf.Lerp(velocity.z, moveVector.z, AirControlFactor);
-        }
+    bool shouldJump = Input.GetKeyDown(KeyCode.Space);
+    Debug.DrawLine(transform.position, transform.position + transform.forward, Color.green);
+    if (isGrounded && shouldJump)
+    {
+      CheckForDefinedMotions();
 
-        bool shouldJump = Input.GetKeyDown(KeyCode.Space);
-        Debug.DrawLine(transform.position, transform.position + transform.forward, Color.green);
-        if (isGrounded && shouldJump)
-        {
-            CheckForDefinedMotions();
+      Velocity.y = JumpForce;
+      ////animator.SetBool(animParamJump, true);
+    }
+    else
+    {
+      Velocity.y -= 9.81f * Time.deltaTime;
+    }
 
-            velocity.y = JumpForce;
-            ////animator.SetBool(animParamJump, true);
-        }
-        else
-        {
-            velocity.y -= 9.81f * Time.deltaTime;
-        }
+    Vector3 preMoveLoc = transform.position;
+    charController.Move(Velocity * Time.deltaTime);
+    Vector3 postMoveLoc = transform.position;
+    Vector3 actualMoveOffset = postMoveLoc - preMoveLoc;
+    Vector3 actualMoveVelocity = actualMoveOffset * (1.0f / Time.deltaTime);
 
-        Vector3 preMoveLoc = transform.position;
-        charController.Move(velocity * Time.deltaTime);
-        Vector3 postMoveLoc = transform.position;
-        Vector3 actualMoveOffset = postMoveLoc - preMoveLoc;
-        Vector3 actualMoveVelocity = actualMoveOffset * (1.0f / Time.deltaTime);
+    // Update velocity if we got blocked somewhere along the way
+    if (Velocity.sqrMagnitude - actualMoveVelocity.sqrMagnitude > 0.01f)
+    {
+      Velocity = actualMoveVelocity;
+    }
 
-        // Update velocity if we got blocked somewhere along the way
-        if (velocity.sqrMagnitude - actualMoveVelocity.sqrMagnitude > 0.01f)
-        {
-            velocity = actualMoveVelocity;
-        }
-
-        isGrounded = false;
-        RaycastHit hitInfo;
-        if (Physics.Raycast(transform.position, new Vector3(0, -1, 0),
+    isGrounded = false;
+    RaycastHit hitInfo;
+    if (Physics.Raycast(transform.position, new Vector3(0, -1, 0),
                                out hitInfo, 1.02f))
-        {
-            isGrounded = true;
-            ////animator.SetBool(animParamJump, false);
-            velocity.y = 0.0f;
-        }
-    }
-
-    private void UpdateOnCurrentMotion()
     {
-        float motionSpeed = 6.0f * Time.deltaTime;
-        Vector3 targetOffset = motionTargets[motionProgress] - transform.position;
-        if (targetOffset.sqrMagnitude < motionSpeed * motionSpeed)
-        {
-            transform.position = motionTargets[motionProgress];
-            ++motionProgress;
-
-            if (motionProgress >= motionTargets.Count)
-            {
-                currentMotion = DefinedMotion.NONE;
-                velocity = new Vector3(0.0f, 0.0f, 0.0f);
-            }
-
-            return;
-        }
-
-        Vector3 targetDirection = targetOffset.normalized;
-        transform.position += targetDirection * motionSpeed;
+      isGrounded = true;
+      ////animator.SetBool(animParamJump, false);
+      Velocity.y = 0.0f;
     }
+  }
 
-    // TODO: May be worthwhile moving movement to FixedUpdate for better physics interaction
-    //       If we do we might want to store movement input each frame lest we miss any
-    private void Update()
+  private void UpdateOnCurrentMotion()
+  {
+    float motionSpeed = 6.0f * Time.deltaTime;
+    Vector3 targetOffset = motionTargets[motionProgress] - transform.position;
+    if (targetOffset.sqrMagnitude < motionSpeed * motionSpeed)
     {
-        if (currentMotion == DefinedMotion.NONE)
-        {
-            UpdateOnPlayerInput();
-        }
-        else
-        {
-            UpdateOnCurrentMotion();
-        }
+      transform.position = motionTargets[motionProgress];
+      ++motionProgress;
+
+      if (motionProgress >= motionTargets.Count)
+      {
+        currentMotion = DefinedMotion.NONE;
+        Velocity = new Vector3(0.0f, 0.0f, 0.0f);
+      }
+
+      return;
     }
+
+    Vector3 targetDirection = targetOffset.normalized;
+    transform.position += targetDirection * motionSpeed;
+  }
+
+  // TODO: May be worthwhile moving movement to FixedUpdate for better physics interaction
+  //       If we do we might want to store movement input each frame lest we miss any
+  private void Update()
+  {
+    if (currentMotion == DefinedMotion.NONE)
+    {
+      UpdateOnPlayerInput();
+    }
+    else
+    {
+      UpdateOnCurrentMotion();
+    }
+  }
 }
---
+++ Assets/Scripts/Respawn/KillPlayerOffMap.cs
@@ -0,0 +1,49 @@
+﻿// <copyright file="KillPlayerOffMap.cs" company="University of Cape Town">
+//     Brian Mc George
+//     MCGBRI004
+// </copyright>
+using System.Collections;
+using UnityEngine;
+
+// TODO: Faciliate for two players
+public class KillPlayerOffMap : MonoBehaviour
+{
+  public float FadeOutSpeed = 1.0f;
+  public float FadeInSpeed = 1.0f;
+  public GUITexture Fadeout;
+  private FirstPersonMovement firstPersonMovement;
+  private bool active;
+
+  internal void Start()
+  {  
+    firstPersonMovement = GetComponent<FirstPersonMovement>();
+    Fadeout.guiTexture.enabled = false;
+    Fadeout.guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height); 
+    Fadeout.guiTexture.color = Color.clear;
+  }
+
+  internal void Update()
+  {
+    if (firstPersonMovement.Velocity.y < -15.0f)
+    {
+      Fadeout.guiTexture.enabled = true;
+      Fadeout.guiTexture.color = Color.Lerp(Fadeout.guiTexture.color, Color.black, FadeOutSpeed * Time.deltaTime);
+      if (Fadeout.guiTexture.color.a >= 0.7f)
+      {
+        active = true;
+        Fadeout.guiTexture.color = Color.black;
+        transform.position = new Vector3(47.01f, 16.143f, 0.0f);
+      }
+    }
+    else if (active)
+    {
+      Fadeout.guiTexture.color = Color.Lerp(Fadeout.guiTexture.color, Color.clear, FadeInSpeed * Time.deltaTime);
+      if (Fadeout.guiTexture.color.a < 0.05f)
+      {
+        active = false;
+        Fadeout.guiTexture.enabled = false;
+        Fadeout.guiTexture.color = Color.clear;
+      }
+    }
+  }
+}
