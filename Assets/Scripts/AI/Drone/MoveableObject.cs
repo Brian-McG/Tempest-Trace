3aa9f1d2f91040361fafeb6e903c73b25e5ea58b
Brian Mc George <mcgbri004@myuct.ac.za>
2015-09-04 13:57:26 +0200

    Sylistic fixes

--- Assets/Scripts/AI/Drone/MoveableObject.cs
+++ Assets/Scripts/AI/Drone/MoveableObject.cs
@@ -5,41 +5,49 @@
 using System.Collections;
 using UnityEngine;
 
-public class MoveableObject {
+public class MoveableObject
+{
   private float moveSpeed;
   private float rotateSpeed;
   private GameObject moveObj;
   private Rigidbody moveRigidbody;
 
   public MoveableObject(float moveSpd,
-                        float rotateSpeed,
-                        GameObject moveableObject) {
-    this.moveObj = moveableObject;
-    this.moveRigidbody = this.moveObj.rigidbody;
-    this.moveSpeed = moveSpd;
-    this.rotateSpeed = rotateSpeed;
+                        float rotSpeed,
+                        GameObject moveableObject)
+  {
+    moveObj = moveableObject;
+    moveRigidbody = this.moveObj.rigidbody;
+    moveSpeed = moveSpd;
+    rotateSpeed = rotSpeed;
   }
 
-  public void Move(Vector3 direction) {
+  public void Move(Vector3 direction)
+  {
     moveRigidbody.AddForce(direction * moveSpeed);
   }
 
-  public void FaceDirection(Vector3 location) {
+  public void FaceDirection(Vector3 location)
+  {
     Vector3 direction = location - moveObj.transform.position;
     float angle = Vector3.Angle(moveObj.transform.forward, location);
 
     float rotate;
 
-    if (angle > 2.0f) {
+    if (angle > 2.0f)
+    {
       rotate = -rotateSpeed;
     }
     else
-    if (angle < -2.0f) {
+    if (angle < -2.0f)
+    {
       rotate = rotateSpeed;
     }
-    else {
+    else
+    {
       rotate = 0.0f;
     }
+
     moveObj.transform.localEulerAngles = new Vector3(
       moveObj.transform.localEulerAngles.x,
       moveObj.transform.localEulerAngles.y + (rotate * Time.deltaTime),
