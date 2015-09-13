a40f2851f638cd5df7c0a2870d8ab41e8e83eb5f
Brian Mc George <mcgbri004@myuct.ac.za>
2015-09-13 22:55:57 +0200

    Add openable doors

---
+++ Assets/Scripts/Door/Door.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="Door.cs" company="University of Cape Town">
+//     Brian Mc George
+//     MCGBRI004
+// </copyright>
+using System.Collections;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+  public float OpenRate;
+  public float DegreesToOpen;
+  private bool status;
+  private bool played;
+  private float startAngle;
+  private float totalOpened;
+
+  public void Activate()
+  {
+    if (!status)
+    {
+      status = true;
+    }
+  }
+
+  internal void Awake()
+  {  
+    status = false;
+    played = false;
+    startAngle = transform.localEulerAngles.y;
+    totalOpened = 0.0f;
+  }
+
+  internal void Update()
+  {
+    if (status && !played && totalOpened < DegreesToOpen)
+    {
+      float deltaAngle = OpenRate * Time.deltaTime;
+      totalOpened += deltaAngle;
+      transform.Rotate(new Vector3(0, deltaAngle, 0));
+    }
+  }
+}
--- Assets/Scripts/Player Controller/InteractHandler.cs
+++ Assets/Scripts/Player Controller/InteractHandler.cs
@@ -8,14 +8,19 @@ public class InteractHandler : MonoBehaviour
 {
   public float InteractDistance;
 
-  private int interactLayer = 1 << 8;
+  private int interactLayer;
   private FirstPersonMovement fpsMove;
   private Transform cameraTransform;
+  private int airconLayer;
+  private int doorLayer;
   
   private void Awake()
   {
     fpsMove = GetComponent<FirstPersonMovement>();
     cameraTransform = GetComponentInChildren<Camera>().transform;
+    airconLayer = LayerMask.NameToLayer("AirconSwitch");
+    doorLayer = LayerMask.NameToLayer("Door");
+    interactLayer = 1 << airconLayer | 1 << doorLayer;
   }
   
   private void Update()
@@ -26,11 +31,23 @@ public class InteractHandler : MonoBehaviour
       if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hitInfo,
                           InteractDistance, interactLayer))
       {
-        GameObject hitObj = hitInfo.transform.parent.gameObject;
-        Aircon airconController = hitObj.GetComponent<Aircon>();
-        if (airconController)
+        if (hitInfo.collider.gameObject.layer == airconLayer)
         {
-          airconController.Activate();
+          GameObject hitObj = hitInfo.transform.parent.gameObject;
+          Aircon airconController = hitObj.GetComponent<Aircon>();
+          if (airconController)
+          {
+            airconController.Activate();
+          }
+        }
+        else if (hitInfo.collider.gameObject.layer == doorLayer)
+        {
+          GameObject hitObj = hitInfo.transform.parent.parent.gameObject;
+          Door doorController = hitObj.GetComponent<Door>();
+          if (doorController)
+          {
+            doorController.Activate();
+          }
         }
       }
     }
