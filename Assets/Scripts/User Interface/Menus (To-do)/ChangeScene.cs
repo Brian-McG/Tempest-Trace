456eb9111342413e58d7d7553dc3a75518bec1a0
Brian Mc George <mcgbri004@myuct.ac.za>
2015-10-11 20:56:56 +0200

    Fix namespace issue

--- Assets/Scripts/User Interface/Menus (To-do)/ChangeScene.cs
+++ Assets/Scripts/User Interface/Menus (To-do)/ChangeScene.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class ChangeScene : MonoBehaviour {
-	public void Start(){
-		Screen.showCursor = true;
-	}
-	// Update is called once per frame
-	public void ChangeToScene (int address) {
-		Application.LoadLevel (address);
-		if (address == 1) {
-			Screen.showCursor = false;
-		}
-	}
-	public void Quit(){
-		Application.Quit ();
-	}
+namespace Domain.UserInterface.Menu
+{
+  public class ChangeScene : MonoBehaviour
+  {
+    public void Start()
+    {
+      Screen.showCursor = true;
+    }
+    // Update is called once per frame
+    public void ChangeToScene(int address)
+    {
+      Application.LoadLevel(address);
+      if (address == 1)
+      {
+        Screen.showCursor = false;
+      }
+    }
+    public void Quit()
+    {
+      Application.Quit();
+    }
+  }
 }
