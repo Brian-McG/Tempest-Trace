ad45b577db70fbf70e4867a412606bce18130012
Brian Mc George <mcgbri004@myuct.ac.za>
2015-10-11 22:43:24 +0200

    Add instructions screen

---
+++ Assets/Scripts/User Interface/Menus (To-do)/Instructions.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="Instructions.cs" company="University of Cape Town">
+//     Brian Mc George
+//     MCGBRI004
+// </copyright>
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Domain.UserInterface.Menu
+{
+  public class Instructions : MonoBehaviour
+  {
+    public Canvas MenuCanvas;
+    public Canvas InstructionsCanvas;
+    private bool showInstructions;
+
+    internal void Update()
+    {
+      if (showInstructions)
+      {
+        MenuCanvas.enabled = false;
+        InstructionsCanvas.enabled = true;
+      }
+      else
+      {
+        MenuCanvas.enabled = true;
+        InstructionsCanvas.enabled = false;
+      }
+      if (Input.GetKeyDown(KeyCode.Escape))
+      {
+        if (showInstructions)
+        {
+          CloseInstructions();
+        }
+      }
+    }
+
+    public void OpenInstructions()
+    {
+      showInstructions = true;
+    }
+    public void CloseInstructions()
+    {
+      showInstructions = false;
+    }
+  }
+}
